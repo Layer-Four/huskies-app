4e122dd0d58e31eb01d0cc9e1316d09368784326
AlexConstandache <alex.constandache@layer-four.de>
2024-01-29 13:56:54 +0100

    App Themes: Update zu login, signup und home helpers, custombuttonauth und shop view finished ticket view und tickets angefangen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:huskies_app/gui/views/auth/login_view.dart';
+import 'package:huskies_app/gui/views/home/home_view.dart';
+import 'package:huskies_app/gui/views/shop/shop_view.dart';
+import 'package:huskies_app/gui/views/ticket_views/ticket_shop_view.dart';
 import 'package:huskies_app/gui/views/widgets/navigation/nav_layer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:huskies_app/logic/provider/notifier.dart';
@@ -26,6 +29,7 @@ class MyApp extends ConsumerWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: state.user != null && state.user!.isLogIn ? const ViewNavigator() : const LoginView(),
+      //home: ViewNavigator(),
     );
   }
 }
