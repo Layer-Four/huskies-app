122b91f711030bb79b80aa905ca4d90487dbae6f
MmeissnerLayer4 <153509730+MmeissnerLayer4@users.noreply.github.com>
2024-02-12 14:59:42 +0100

    write product model

---
+++ lib/models/products_model/product.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'product.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$ProductImpl _$$ProductImplFromJson(Map<String, dynamic> json) =>
+    _$ProductImpl(
+      imageUrl: json['imageUrl'] as String,
+      price: json['price'] as int,
+      shopUrl: json['shopUrl'] as String,
+      title: json['title'] as String,
+    );
+
+Map<String, dynamic> _$$ProductImplToJson(_$ProductImpl instance) =>
+    <String, dynamic>{
+      'imageUrl': instance.imageUrl,
+      'price': instance.price,
+      'shopUrl': instance.shopUrl,
+      'title': instance.title,
+    };
