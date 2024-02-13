75dfa7ccc57da55a6de6f0ca2c552b74cdc4d5b1
obaidajarrah <153510718+obaidajarrah@users.noreply.github.com>
2024-02-13 11:48:37 +0100

    implement web view

--- lib/views/view_widgets/newsfeed/newsfeed.dart
+++ lib/views/view_widgets/newsfeed/newsfeed.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:huskies_app/views/view_widgets/newsfeed/navigation_controlls.dart';
+import 'package:huskies_app/views/view_widgets/newsfeed/web_view_stack.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+
+// import 'src/navigation_controls.dart';
+// import 'src/web_view_stack.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: NewsFeed(),
+    ),
+  );
+}
 
 class NewsFeed extends StatefulWidget {
   const NewsFeed({super.key});
@@ -9,33 +22,31 @@ class NewsFeed extends StatefulWidget {
 }
 
 class _NewsFeedState extends State<NewsFeed> {
-  late WebViewController _controller;
-  String pageTitle = '-';
-  double progress = 0;
+  // Add from here...
+  late final WebViewController controller;
 
   @override
   void initState() {
     super.initState();
-    _controller = WebViewController();
-    _controller.setJavaScriptMode(JavaScriptMode.disabled);
-    _controller.loadRequest(
-        Uri.parse('https://www.kassel-huskies.de/saison/spielplan'));
+    controller = WebViewController()
+      ..loadRequest(
+        Uri.parse('https://www.kassel-huskies.de/news'),
+      );
   }
+  // ...to here.
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: WebViewWidget(
-                controller: _controller,
-              ),
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        title: const Text('Flutter WebView'),
+        // Add from here...
+        actions: [
+          NavigationControls(controller: controller),
+        ],
+        // ...to here.
       ),
+      body: WebViewStack(controller: controller),
     );
   }
 }
