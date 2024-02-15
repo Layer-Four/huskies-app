e35d2837d5c7a4b1e0841d8426b3035dd761e7b0
MmeissnerLayer4 <153509730+MmeissnerLayer4@users.noreply.github.com>
2024-02-15 09:32:29 +0100

    delete create user-> route to login in screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:huskies_app/provider/auth_provider/auth_notifier.dart';
+import 'package:huskies_app/provider/auth_provider/auth_state_provider.dart';
+import 'package:huskies_app/provider/error_provider/error_provider.dart';
+import 'package:huskies_app/provider/static_provider.dart';
 import 'package:huskies_app/services/firebase_options.dart';
+import 'package:huskies_app/views/error_view/error_screen_widget.dart';
+import 'package:huskies_app/views/loading_view.dart';
 import 'package:huskies_app/views/navigation_view/nav_layer.dart';
+import 'package:huskies_app/views/wait_registry_view/wait_register_widget.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,23 +33,23 @@ class MyApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, ref) {
-    return const ViewNavigator();
-    // return switch (ref.watch(statusProvider)) {
-    //   AuthState.loggedIn => ref.watch(authProvider).when(
-    //         loading: () => const LoadingView(),
-    //         error: (error, stackTrace) {
-    //           log('a Error occurrences: ${error.toString()} \n this was the path: $stackTrace');
-    //           final errors = ref.watch(errorProvider);
-    //           return ShowErrorScreen(
-    //             error: 'error: ${errors.last}',
-    //           );
-    //         },
-    //         data: (user) => const ViewNavigator(),
-    //       ),
-    //   AuthState.loading => const LoadingView(),
-    //   AuthState.onRegistration => const WaitForRegistry(),
-    //   AuthState.onError => ShowErrorScreen(error: ref.watch(errorProvider).last),
-    //   _ => const LoginView(),
-    // };
+    // return const ViewNavigator();
+    return switch (ref.watch(statusProvider)) {
+      AuthState.loggedIn => ref.watch(authProvider).when(
+            loading: () => const LoadingView(),
+            error: (error, stackTrace) {
+              log('a Error occurrences: ${error.toString()} \n this was the path: $stackTrace');
+              final errors = ref.watch(errorProvider);
+              return ShowErrorScreen(
+                error: 'error: ${errors.last}',
+              );
+            },
+            data: (user) => const ViewNavigator(),
+          ),
+      AuthState.loading => const LoadingView(),
+      AuthState.onRegistration => const WaitForRegistry(),
+      AuthState.onError => ShowErrorScreen(error: ref.watch(errorProvider).last),
+      _ => const ViewNavigator(),
+    };
   }
 }
